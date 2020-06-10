f6889a6163402bc2c58b91e641e0791e8ac79738
koshio3112 <ponyo0813131@gmail.com>
2020-06-10 21:43:33 +0900

    credential.yml edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials[:basic_auth][:teamf] &&
-      password == Rails.application.credentials[:basic_auth][:freemarketf]
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
     end
   end
 
