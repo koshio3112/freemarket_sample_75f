6e187d6df69d501a20c2b35e451f17b8f2f7c9d8
koshio3112 <ponyo0813131@gmail.com>
2020-06-10 20:44:59 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.0"
 
 set :application, "freemarket_sample_75f"
-set :repo_url, "git@github.com:perunari/freemarket_sample_75f.git"
+set :repo_url, "git@github.com:koshio3112/freemarket_sample_75f.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
